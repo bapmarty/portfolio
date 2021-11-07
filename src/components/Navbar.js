70527944bc2a8d389cd40ee5e25e645bc86042c8
baptiste.martin <baptiste.matin@fabrick.io>
2021-11-07 17:23:54 +0100

    :lipstick: Remove fixed navbar to top

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,34 +5,14 @@ import {useEffect, useState} from "react";
 import {Link} from "react-scroll";
 
 const Navbar = () => {
-
-  const [color, setColor] = useState("dark");
-  const [background, setBackground] = useState("");
-
   const [theme, setTheme] = useState("light");
 
-  useEffect(() => {
-    const onScroll = e => {
-      console.log(e.target.documentElement.scrollTop);
-      if (e.target.documentElement.scrollTop < 820) {
-        setColor("dark");
-        setBackground("");
-      }
-      else if (e.target.documentElement.scrollTop >= 820) {
-        setColor("white");
-        setBackground("bg-purple");
-      }
-    };
-    window.addEventListener("scroll", onScroll);
-
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [color, background]);
   return (
     <>
-      <section className={"navbar-header " + background}>
+      <section className={"navbar-header"}>
         <ul className={"left-nav"}>
         </ul>
-        <ul className={color + " right-nav"}>
+        <ul className={"right-nav"}>
           <li><Link smooth={true} duration={400} to={"home"}><FontAwesomeIcon icon={faHome}/> Home</Link></li>
           <li><Link smooth={true} duration={500} to={"about"}><FontAwesomeIcon icon={faSignature} /> About Me</Link></li>
           <li><a href="https://github.com/baptistemrt/"><FontAwesomeIcon icon={faGithub}/> Github</a></li>
